d48bcfe35c1b07dd7f637d5835315e8bacf33580
ecourse <smkang@codenuri.co.kr>
2022-11-21 09:52:31 +0900

    Add files via upload

--- DAY1/1_constructor2-2.cpp
+++ DAY1/1_constructor2-2.cpp
@@ -19,6 +19,10 @@ public:
 	Stream(Buffer& buf) { buf.useBuffer(); }
 };
 //===========================================
+// 아래 코드는 "C++ 표준 ostream" 을 만들때 나왔던 문제와해결책입니다.
+// "Base From Member" 라는 이름을 가진 기술 입니다.
+// 구글에서 "C++ IDioms" 라고 검색해보세요 - 1번째 링크 선택
+// 
 // Stream 보다 먼저 초기화 하고 싶은 데이타를 모아 놓은
 // 별도의 클래스를 설계합니다.
 class PipeBuffer
@@ -27,7 +31,7 @@ protected:
 	Buffer buff{ 1024 };
 };
 
-class PipeStream : public PipeBuffer, public Stream
+class PipeStream : protected PipeBuffer, public Stream
 {
 public:
 	PipeStream() : Stream(buff) {}
