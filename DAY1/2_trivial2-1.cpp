65ea077241bed2e20493d7ed06500a22fdf39770
ecourse <smkang@codenuri.co.kr>
2022-11-21 11:33:00 +0900

    Add files via upload

---
+++ DAY1/2_trivial2-1.cpp
@@ -0,0 +1,26 @@
+// 2_trivial2-1
+#include <iostream>
+#include <type_traits>
+
+// POD : Plain Old Data, C언어의 구조체와 호환 되는가 ?
+//		 C++98 에 사용되는 용어, C++11 부터는 사용안함.
+
+// C++11 이후
+// trivial		   : 특별 함수가 하는일이 널리 알려진 대로 동작하는가 ?
+//					 함수 동작에 대한 C와의 호환성
+// standard layout : 메모리 구조가 C언어와 호환되는가 ?
+
+struct SL_NOT_TRIVIAL
+{
+	int x, y;
+	SL_NOT_TRIVIAL() {}
+};
+int main()
+{
+	std::cout << std::is_trivially_default_constructible_v<SL_NOT_TRIVIAL> << std::endl;
+	std::cout << std::is_standard_layout_v<SL_NOT_TRIVIAL> << std::endl;
+
+}
+
+
+
