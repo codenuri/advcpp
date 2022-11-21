9034d315e1d1d663fd3fd51632aa5aa715066b58
ecourse <smkang@codenuri.co.kr>
2022-11-21 09:38:46 +0900

    Add files via upload

---
+++ DAY1/1_constructor2-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// Base-from-Member
+
+class Buffer
+{
+	char* buff;
+public:
+	Buffer(int size) : buff(new char[size])
+	{
+		std::cout << "버퍼 크기 " << size << "로 초기화" << std::endl;
+	}
+	void useBuffer() { std::cout << "버퍼 사용" << std::endl; }
+};
+
+class Stream
+{
+public:
+	Stream(Buffer& buf) { buf.useBuffer(); }
+};
+//===========================================
+
+class PipeStream : public Stream
+{
+	Buffer buff{ 1024 };
+public:
+	PipeStream() : Stream(buff) {}
+};
+
+int main()
+{
+	PipeStream ps;
+}
