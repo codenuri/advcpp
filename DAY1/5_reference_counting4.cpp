9cf52a1972f602db850f40f7ac5edf213869b349
ecourse <smkang@codenuri.co.kr>
2022-11-21 15:20:39 +0900

    Add files via upload

--- DAY1/5_reference_counting4.cpp
+++ DAY1/5_reference_counting4.cpp
@@ -7,10 +7,20 @@ class RefCount
 	int refCount = 0;
 public:
 	void AddRef() { ++refCount; }
-	void Release() { if (--refCount == 0) delete this; }
+
+	void Release()	// void Release( RefCount* this)
+	{
+		if (--refCount == 0) 
+			delete this; // 즉, 이순간 this 의 타입은 RefCount* 입니다.
+						// 소멸자가 가상이 아니면 "~RefCount()" 만 호출
+						// 소멸자가 가상이어야 실제 객체를 조사후 호출
+	}
 protected:
-	~RefCount() { std::cout << "~RefCount" << std::endl; }
+//	~RefCount() { std::cout << "~RefCount" << std::endl; }
+	virtual ~RefCount() { std::cout << "~RefCount" << std::endl; }
 };
+
+
 // Truck 도 참조 계수로 관리하고 싶다.
 class Truck : public RefCount
 {
