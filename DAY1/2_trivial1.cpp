7a8e4738b0c63e2ab8dfe328cbbb375a742b34cb
ecourse <smkang@codenuri.co.kr>
2022-11-21 11:03:16 +0900

    Add files via upload

---
+++ DAY1/2_trivial1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+// trivial(자명하다)
+// => special member function 이 하는 일이 널리 알려진 대로 동작!
+// => 컴파일러의 기본 제공 버전을 사용하는 가 ?
+
+// 생성자, 소멸자 : 아무 일도 하지 않을때 trivial
+// 복사 생성자    : 모든 멤버를 얕은 복사할때 trivial
+
+class A
+{
+};
+
+class B
+{
+public:
+	virtual void foo() {}
+};
+int main()
+{
+
+}
