56b4a9ced3ac6f87f5ca1ece8e261561efac0725
ecourse <smkang@codenuri.co.kr>
2022-11-21 10:28:04 +0900

    Add files via upload

---
+++ DAY1/1_constructor4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+// 생성자와 예외
+
+// 핵심 1. 생성자에서 작업을 하다가 실패한다면 ??
+//       => 생성자는 반환 값을 가질수 없다.
+//		 => 일반적으로는 "예외" 를 통해서 실패를 전달한다.
+
+struct Resource
+{
+	Resource()  { std::cout << "acquire Resource" << std::endl; }
+	~Resource() { std::cout << "release Resource" << std::endl; }
+};
+
+class Test
+{
+	Resource* p;
+public:
+	Test() : p(new Resource)
+	{
+		std::cout << "Test()" << std::endl;
+
+		throw 1; // 실패 했다고 가정하고 예외 전달
+	}
+
+	~Test() 
+	{ 
+		delete p;
+		std::cout << "~Test()" << std::endl;
+	}
+};
+int main()
+{
+	try
+	{
+		Test t;
+	}
+	catch (...)
+	{
+		std::cout << "Test 생성자에서 예외 발생" << std::endl;
+	}
+	std::cout << "프로그램은 계속 실행" << std::endl;
+}
