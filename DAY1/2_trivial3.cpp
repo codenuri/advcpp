84dc9276fae49d1da0ecbf189ad8f12474f06f28
ecourse <smkang@codenuri.co.kr>
2022-11-21 11:48:21 +0900

    Add files via upload

---
+++ DAY1/2_trivial3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+struct Point
+{
+	int x, y;
+
+	Point() = default;
+	Point(int a, int b) : x(a), y(b) {}
+};
+
+template<typename T>
+void copy(T* dst, T* src, std::size_t sz)
+{
+	if (std::is_trivially_copyable_v<T>)
+	{
+		std::cout << "using memcpy" << std::endl;
+		memcpy(dst, src, sizeof(T) * sz);
+	}
+	else
+	{
+		std::cout << "using copy constructor" << std::endl;
+		while (sz--)
+		{
+			new(dst) T(*src); // 복사 생성자 호출 - 수요일 정도에 배웁니다.
+			++dst, ++src;
+		}
+	}
+}
+
+int main()
+{
+	Point pt1[5] = { {1,1}, {2,2}, {3,3}, {4,4}, {5,5} };
+	Point pt2[5];
+	copy(pt2, pt1, 5);
+
+}
