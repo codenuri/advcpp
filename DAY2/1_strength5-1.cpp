66a341dbe890b5502c5696c94e1fb250ce7370aa
ecourse <smkang@codenuri.co.kr>
2022-11-22 09:54:49 +0900

    Add files via upload

--- DAY2/1_strength5-1.cpp
+++ DAY2/1_strength5-1.cpp
@@ -1,7 +1,7 @@
 
 // 인자가 없거나 여러개인 경우도 explicit 붙이는 경우가 많습니다.
-struct empty1 {	empty1() {} };
-struct empty2 { explicit empty2() {} };
+struct empty1 {	empty1() = default;};
+struct empty2 { explicit empty2() = default; };
 
 struct Point1 {	Point1(int a, int b) {} };
 struct Point2 { explicit Point2(int a, int b) {} };
@@ -13,6 +13,9 @@ void f4(Point2 p1) {}
 
 int main()
 {
-	f1({});
-	f2({});
+	f1({}); // ok
+	f2({}); // error. empty2 는 explicit
+
+	f3({ 1,2 }); // ok
+	f4({ 1,2 }); // error.
 }
