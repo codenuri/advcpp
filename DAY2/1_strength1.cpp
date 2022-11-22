5cc0fdda0fbc6c73e782fb5fdbd3ef8173d07fcf
ecourse <smkang@codenuri.co.kr>
2022-11-22 09:08:24 +0900

    Add files via upload

--- DAY2/1_strength1.cpp
+++ DAY2/1_strength1.cpp
@@ -5,6 +5,8 @@ const T& max1(const T& lhs, const T& rhs)
 {
 	return lhs < rhs ? rhs : lhs;
 }
+
+// attribute : 컴파일러에게 지시하는 명령문
 // [[nodiscard]]: 반환 값을 무시하는 경우 경고를 발생시켜 달라는 "attribute"
 //                C++17
 template<typename T>
@@ -17,4 +19,7 @@ int main()
 {
 	max1(1, 2);	
 	max2(1, 2);
+	
+	printf("AAA"); // 반환값도 있지만 대부분 사용하지 않습니다.
+				   // 반환값이 중요한 것은 아닙니다.
 }
