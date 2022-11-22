1f21642df874af86b661d4a2a80992cdc477c202
ecourse <smkang@codenuri.co.kr>
2022-11-22 15:14:17 +0900

    Add files via upload

--- DAY2/2_thiscall7-1.cpp
+++ DAY2/2_thiscall7-1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <functional>
 // 멤버 변수 포인터
 struct Point
 {
@@ -12,6 +12,21 @@ int main()
 	int Point::*p1 = &Point::y; 
 
 	// 핵심 1. "pointer to member data" 사용하기
-	*p1 = 20;	
+//	*p1 = 20;	// error. 객체가 없다.
 
+	Point pt;
+	
+	pt.*p1 = 20;	 // ok.  pt.y = 20 과 동일
+					 // => *(int*)((char*)&pt + p1) = 20;
+
+	(&pt)->*p1 = 20; // ok
+
+
+	// 핵심 2. 멤버 "데이타" 포인터도 std::invoke 가능합니다.
+	
+	int n = std::invoke(p1, pt); // pt.*p1
+
+	std::invoke(p1, pt) = 30;	// pt.*p1 = 30
+
+	std::cout << pt.y << std::endl;
 }
