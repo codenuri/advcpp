aa2b4e89be8cc501d8b546bbc748d6c8add1d8a0
ecourse <smkang@codenuri.co.kr>
2022-11-22 11:28:17 +0900

    Add files via upload

--- DAY2/2_thiscall2-2.cpp
+++ DAY2/2_thiscall2-2.cpp
@@ -1,3 +1,5 @@
+#include <functional>
+
 class Dialog
 {
 public:
@@ -18,6 +20,24 @@ int main()
 	f1(10);
 	(dlg.*f2)(10);	
 
+	// 핵심 2. std::invoke 를 사용하면 동일한 표현식으로 사용할수 있습니다.
+	// => C++17
+	std::invoke(f1, 10);      // 일반함수포인터 사용
+	std::invoke(f2, dlg, 10); // 멤버함수포인터 사용
+
+
+	// 핵심 3. std::mem_fn 을 사용하면 멤버 함수의 주소를 보관했다가
+	//  ()로 호출가능합니다. - C++11
+	auto f3 = std::mem_fn(&Dialog::Close);
+
+	f3(dlg, 10); 
+//	(dlg.*f3)(10); // f3 인 raw pointer 였다면 이렇게 했어야 합니다.
+
+	// 참고 : 멤버 함수의 주소를 구할때는 반드시 &를 붙여야 합니다.
+	f1 = foo; // 이렇게해도 되고
+	f1 = &foo; // 이 표기도 가능
+	f2 = Dialog::Close; // error. & 가 반드시 필요 
+	f2 = &Dialog::Close; // ok
 }
 
 
