c683a132ec40ec740de61789650e6ca63ce6b89c
ecourse <smkang@codenuri.co.kr>
2022-11-22 16:18:50 +0900

    Add files via upload

---
+++ DAY2/4_max6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <functional>
+#include <string>
+
+struct identity
+{
+	template<typename T>
+	constexpr T&& operator()(T&& obj) const noexcept
+	{
+		return std::forward<T>(obj);
+	}
+};
+
+template<typename T, typename Projection = identity>
+const T& mymax(const T& lhs, const T& rhs, Projection proj = {})
+{
+	return std::invoke(proj, lhs) < std::invoke(proj, rhs) ? rhs : lhs;
+}
+
+int main()
+{
+	std::string s1 = "AAAAA";
+	std::string s2 = "BBB";
+
+	auto ret1 = mymax(s1, s2, [](const std::string& s) { return s.size(); });
+}
