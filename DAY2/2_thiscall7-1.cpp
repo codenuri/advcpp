38cdfc5e913ef6cf1abe8272fae6d2d635f5ea9f
ecourse <smkang@codenuri.co.kr>
2022-11-22 15:17:58 +0900

    Add files via upload

--- DAY2/2_thiscall7-1.cpp
+++ DAY2/2_thiscall7-1.cpp
@@ -30,3 +30,13 @@ int main()
 
 	std::cout << pt.y << std::endl;
 }
+
+// 함수 객체(function object)
+// => () 연산자를 사용해서 호출 가능한 모든 객체
+// => 함수, 함수포인터, 멤버 함수, 멤버함수 포인터,
+//    ()연산자를 재정의한 클래스의 객체, 람다 표현식등
+
+// Callable Object ( 호출가능한 객체 ) <= 이 용어가 더 중요합니다.
+// => std::invoke() 가 가능한 객체
+// => function object + 멤버데이타 포인터
+
