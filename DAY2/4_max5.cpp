a8689d769ade71c28aa7efdbc75f76a9190dcc57
ecourse <smkang@codenuri.co.kr>
2022-11-22 16:17:37 +0900

    Add files via upload

--- DAY2/4_max5.cpp
+++ DAY2/4_max5.cpp
@@ -11,8 +11,8 @@ struct identity
 	}
 };
 
-template<typename T, typename Projection>
-const T& mymax(const T& lhs, const T& rhs, Projection proj)
+template<typename T, typename Projection = identity>
+const T& mymax(const T& lhs, const T& rhs, Projection proj = {} )
 {
 	return std::invoke(proj, lhs) < std::invoke(proj, rhs) ? rhs : lhs;
 }
@@ -24,4 +24,14 @@ int main()
 
 	auto ret1 = mymax(s1, s2, [](const std::string& s) { return s.size(); });
 
+
+	// 결국 위 코드는 s1, s2를 직접 비교하지 않고, Projection 된 결과를 비교 합니다.
+
+	// identity 는 Projection 의 결과가 결국 자기 자신 입니다.
+	identity f;
+	auto ret2 = mymax(s1, s2, f );
+				// f(s1) < f(s2) 인데, 결국 "s1 < s2" 
+			
+	// 따라서, identity 를 mymax 의 3번째 인자의 디폴트 값으로 채택하면 됩니다.
+	auto ret3 = mymax(s1, s2); // 디폴트 Projection (identity 사용)
 }
