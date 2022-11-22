782cbe67d42f8d02221311057a6476ee5834da4c
ecourse <smkang@codenuri.co.kr>
2022-11-22 14:18:41 +0900

    Add files via upload

---
+++ DAY2/2_thiscall4-1.cpp
@@ -0,0 +1,34 @@
+// 2_thiscall4-1 - 5번 복사
+#include <iostream>
+
+struct A
+{
+	int a;
+	void fa() { std::cout << "fa : " << this << std::endl; }
+};
+struct B
+{
+	int b;
+	void fb() { std::cout << "fb : " << this << std::endl; }
+};
+struct C : public A, public B
+{
+	int c;
+	void fc() { std::cout << "fc : " << this << std::endl; }
+};
+int main()
+{
+	C obj;
+
+	std::cout << &obj << std::endl; // 1000 번지 일때 아래 3줄 예측하세요
+
+	// 아래 3줄의 결과로  출력되는 주소 예측해 보세요
+	obj.fa();	// ?
+	obj.fb();	// ?
+	obj.fc();	// ?
+
+}
+
+
+
+
