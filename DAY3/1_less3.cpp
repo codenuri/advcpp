86e4de93339ca44f2d7bc63441e857274cbdd977
ecourse <smkang@codenuri.co.kr>
2022-11-23 09:34:06 +0900

    Add files via upload

--- DAY3/1_less3.cpp
+++ DAY3/1_less3.cpp
@@ -8,4 +8,14 @@ int main()
 	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
 
 	std::sort(v.begin(), v.end(), std::less<int>()); // C++98 스타일
+
+	// 아래 코드는 모두 같은 의미 입니다.
+	std::sort(v.begin(), v.end(), std::less<void>()); // C++14 스타일
+	std::sort(v.begin(), v.end(), std::less<>());
+	std::sort(v.begin(), v.end(), std::less()); // C++17
+	std::sort(v.begin(), v.end(), std::less{}); //
+	std::sort(v.begin(), v.end(), {}); 
 }
+
+
+
