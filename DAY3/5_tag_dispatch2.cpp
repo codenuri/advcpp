edf76d7480f0ffaa115cfc1a7b3d09750de10443
ecourse <smkang@codenuri.co.kr>
2022-11-23 13:49:54 +0900

    Add files via upload

--- DAY3/5_tag_dispatch2.cpp
+++ DAY3/5_tag_dispatch2.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <mutex>
 
-
-
 // 생성자 함수 오버로딩을 위해서 empty class 타입을 설계
-struct adopt_lock_t {};
+// => 컴파일러가 만드는 "디폴트 생성자는 explicit" 가 아닙니다.
+// => 디폴트 생성자를 explicit 로 하려면 사용자가 명시적으로 만들어야 합니다.
 
-adopt_lock_t adopt_lock;
+struct adopt_lock_t { explicit adopt_lock_t() = default; };
+
+constexpr adopt_lock_t adopt_lock;
+
+//============================================================
 
 template <typename Mutex> class lock_guard
 {
@@ -23,7 +26,13 @@ int main()
 {
     std::mutex m;
 
-    lock_guard<std::mutex> g1(m);               // m.lock()
-    lock_guard<std::mutex> g2(m, adopt_lock);   // 생성자에서 lock 수행 안함.
+    //  lock_guard<std::mutex> g1(m);               // m.lock()
+    //  lock_guard<std::mutex> g2(m, adopt_lock);   // 생성자에서 lock 수행 안함.
+ 
+    lock_guard<std::mutex> g2(m, {});   // 되는게 좋은세요 ? 안되는게 좋을까요?
+   
+
+    // 이미 C++ 표준에 lock_guard, adopt_lock 모두 있습니다.
+    std::lock_guard<std::mutex> g3(m, std::adopt_lock);
 }
 
