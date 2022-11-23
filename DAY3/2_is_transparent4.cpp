905baa174ea2c66dc941123be939e4eb3ec8907d
ecourse <smkang@codenuri.co.kr>
2022-11-23 11:17:48 +0900

    Add files via upload

--- DAY3/2_is_transparent4.cpp
+++ DAY3/2_is_transparent4.cpp
@@ -20,3 +20,30 @@ class People
 	// friend operator<(People, string)
 	// friend operator<(string, People)
 };
+std::set<People, std::less<void>> s2;
+
+// 방법1 과 방법2의 차이점은 ...
+// 결국 "방법2" 는 People 자체에 < 연산이 됩니다.
+// => set 에 넣는것 외에도 비교할일이 많이 있다면 당연히 2번이 좋습니다.
+People p1, p2;
+bool b = p1 < p2; // 방법 2만 가능.
+
+//================
+// std::set<int, less<int>> 어떻게 less 만 가지고 가능한가요 ? greater 도있어야
+// 하지 않나요 ?
+void insert(const T& newelem)
+{
+	if (pr(root, newelem)) // root < newelem
+	{
+		// 오른쪽으로 이동
+	} 
+	else if (pr(newelem, root)) //  newelem < root
+	{
+		// 왼쪽이동
+	}
+	else
+	{
+		// 이미 요소가 있다.
+	}
+
+}
