5076eb1fc8b2fecca99a55be4cbaab0891a61708
ecourse <smkang@codenuri.co.kr>
2022-11-23 09:27:25 +0900

    Add files via upload

--- DAY3/1_less2.cpp
+++ DAY3/1_less2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // 아래 구현은 C++14 버전에서 변경된 내용입니다.
-template <typename T>
+template <typename T = void>
 struct less
 {
     inline bool operator()(const T& lhs, const T& rhs) const
@@ -15,16 +15,26 @@ template <>
 struct less<void>
 {
     template<typename T1, typename T2>
-    inline bool operator()(T1&& lhs,T2&& rhs) const
+    [[nodiscard]] constexpr auto operator()(T1&& lhs,T2&& rhs) const 
+        noexcept( noexcept(std::forward<T1>(lhs) < std::forward<T2>(rhs)) )
+        -> decltype(std::forward<T1>(lhs) < std::forward<T2>(rhs))
     {
-        return lhs < rhs;
+        return std::forward<T1>(lhs) < std::forward<T2>(rhs);
     }
 };
 
 int main()
 {
-    less<void> f;
-    bool ret = f(10, 10.5);
+    less<int> f1;
+    bool ret1 = f1(10, 10.5); // false !
 
-    std::cout << ret << std::endl;
+    //----------------------------------
+//  less<void> f2;
+//  less<>     f2; // 타입인자를 전달하지 않았으므로 디폴트값(void 사용)
+    less       f2; // C++17 부터는 <> 자체도 생략가능
+                   // 결국 이 표기법이 less<void> 를 사용하겠다는 의미
+
+    bool ret2 = f2(10, 10.5);   // true
+
+    std::cout << ret2 << std::endl;
 }
