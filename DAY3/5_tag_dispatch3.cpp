3eb9d61b47dad61cab3ec700e30c6e62c32abced
ecourse <smkang@codenuri.co.kr>
2022-11-23 14:14:49 +0900

    Add files via upload

--- DAY3/5_tag_dispatch3.cpp
+++ DAY3/5_tag_dispatch3.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <new>
 
+
+void* operator new(std::size_t sz)
+{
+	std::cout << "operator new : " << sz << std::endl;
+
+	void* p = malloc(sz);
+
+	if (p == nullptr)
+		throw std::bad_alloc();
+
+	return p;
+}
+// new(std::nonthrow) int[10] 
+// => operator new(sizeof(int[10]), std::nonthrow)
+
+struct nothrow_t {};
+nothrow_t mynothrow;
+
+void* operator new(std::size_t sz, nothrow_t) noexcept
+{
+	std::cout << "operator new(nothrow) : " << sz << std::endl;
+
+	void* p = malloc(sz);
+
+	return p; // 실패하면 0반환
+}
+
 int main()
 {
 	try
@@ -12,12 +39,11 @@ int main()
 	{
 	}
 	//-------------------------------------------
-	int* p2 = new(nothrow) int[20]; // 실패시 예외가 아닌 0반환
+//	int* p2 = new(std::nothrow) int[20]; // 실패시 예외가 아닌 0반환
+	int* p2 = new(mynothrow) int[20]; // 실패시 예외가 아닌 0반환
 
 	if (p2 == nullptr) {}
 	
 	delete[] p2;
-
-
 
 }
