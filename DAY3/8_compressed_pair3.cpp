c94ae2bd165fa61f93d8bbfa997f329cfd529fbe
ecourse <smkang@codenuri.co.kr>
2022-11-23 16:30:59 +0900

    Add files via upload

--- DAY3/8_compressed_pair3.cpp
+++ DAY3/8_compressed_pair3.cpp
@@ -5,6 +5,13 @@
 class Empty {};
 
 
+struct Point
+{
+    int x, y;
+    Point(int a, int b) : x(a), y(b) {}
+};
+
+
 template<typename T1, typename T2, bool = std::is_empty_v< T1  > >
 struct compressed_pair;
 
@@ -21,21 +28,18 @@ struct compressed_pair<T1, T2, false>
     const T1& get_first() const { return first; }
     const T2& get_second()const { return second; }
 
-    template<typename U1, typename U2>
-    compressed_pair(U1&& a, U2&& b)
-        : first(std::forward<U1>(a)), second(std::forward<U2>(b))
+    template<typename U1, typename ... U2>
+    compressed_pair(U1&& a, U2&& ... args)
+        : first(std::forward<U1>(a)), second(std::forward<U2>(args)...)
     {}
-};
-
-struct Point
-{
-    int x, y;
-    Point(int a, int b) : x(a), y(b) {}
 };
 
 int main()
 {
-    compressed_pair<int, Point> p1(3, Point(1, 2));
+    compressed_pair<int, Point> p1(3, Point(1, 2) );
+
+    compressed_pair<int, Point> p2(3, 1, 2); // Point 만들때 필요한 생성자
+                                            // 인자를 전달
 
     
 }
