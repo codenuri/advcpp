fc4cce432e237bae5b293e9d2e0c2ba230a6fb69
ecourse <smkang@codenuri.co.kr>
2022-11-23 10:37:44 +0900

    Add files via upload

---
+++ DAY3/2_is_transparent2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+struct People
+{
+	std::string name;
+	int age;
+
+	People(const std::string& name, int age) : name(name), age(age) {}
+};
+
+
+int main()
+{
+	std::set<People> s;	
+
+	s.emplace("ddd", 20);
+	s.emplace("bbb", 30);
+	s.emplace("ccc", 40);
+
+	auto ret1 = s.find(People("bbb", 30)); 
+
+	auto ret2 = s.find("bbb"); 
+}
