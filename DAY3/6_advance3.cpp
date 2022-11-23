8711d1d8101c8bdead3f02e01f866672a6ed73cb
ecourse <smkang@codenuri.co.kr>
2022-11-23 15:11:18 +0900

    Add files via upload

--- DAY3/6_advance3.cpp
+++ DAY3/6_advance3.cpp
@@ -8,11 +8,9 @@
 // => enable_if_t 로 조건을만족하는 경우만 사용
 
 template<typename T>
-
 std::enable_if_t< std::is_same_v< 
 			typename std::iterator_traits<T>::iterator_category, 
 			std::random_access_iterator_tag>  >
-
 xadvance(T& p, int sz)
 {
 	std::cout << "+ 로 이동" << std::endl;
@@ -20,11 +18,9 @@ xadvance(T& p, int sz)
 }
 
 template<typename T>
-
 std::enable_if_t< !std::is_same_v<
 	typename  std::iterator_traits<T>::iterator_category,
 	std::random_access_iterator_tag>  >
-
 xadvance(T& p, int sz)
 {
 	std::cout << "++ 로 이동" << std::endl;
