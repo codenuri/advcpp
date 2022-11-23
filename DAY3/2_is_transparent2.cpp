b7bfa75c6a8b196d1e850661bb4893e1e7fa2a1c
ecourse <smkang@codenuri.co.kr>
2022-11-23 10:43:18 +0900

    Add files via upload

--- DAY3/2_is_transparent2.cpp
+++ DAY3/2_is_transparent2.cpp
@@ -9,11 +9,22 @@ struct People
 
 	People(const std::string& name, int age) : name(name), age(age) {}
 };
+// People 자체에 < 연산이 가능하다면 set 에 넣을때 기본 함수 객체인 std::less도
+// 가능합니다.
+bool operator<(const People& p1, const People& p2) { return p1.name < p2.name;}
+bool operator<(const People& p1, const std::string& name) {	return p1.name < name;}
+bool operator<(const std::string& name, const People& p2) {	return name < p2.name;}
+
+// 결론 : People 자체는 
+// "People 객체 < People 객체"
+// "People 객체 < 문자열(이름)"
+// "문자열(이름) < People 객체"
 
 
 int main()
 {
-	std::set<People> s;	
+	std::set<People> s;	// std::set<People, std::less<People>> 
+
 
 	s.emplace("ddd", 20);
 	s.emplace("bbb", 30);
