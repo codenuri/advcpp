4f1417b3646f25ff92f099485dbbbfbef24abf41
ecourse <smkang@codenuri.co.kr>
2022-11-24 11:10:59 +0900

    Add files via upload

--- DAY4/1_new7.cpp
+++ DAY4/1_new7.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+class Point
+{
+	int x;
+	int y;
+public:
+	Point() : x(0), y(0) { std::cout << "Point()" << std::endl; }
+	Point(int a, int b) : x(a), y(b) { std::cout << "Point(int, int)" << std::endl; }
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+
 // 사용자 정의 allocator 만들기
 // => C++ 표준에서 정의한 규칙을 반드시 따라야 합니다.
 
@@ -21,20 +31,22 @@ public:
 	}
 	void destroy(T* p)
 	{
+		printf("call destructor %p\n", p);
 		p->~T();
 	}
 	// 생성자 호출을 위한 construct 도 필요 - 좀 어렵습니다.
 	template<typename ... ARGS>
 	void construct(T* p, ARGS&& ... args)
 	{
+		printf("call constructor %p\n", p);
 		new(p) T(std::forward<ARGS>(args)...);
 	}
-
 	// 규칙 2. 아래 3개의 멤버도 필요 합니다.(관례적인 코드)
 	using value_type = T;
 	debug_alloc() = default;
 	template<typename U> debug_alloc(const debug_alloc<U>& ) {}
 };
+
 // 규칙 3. == 와 != 연산자 제공되어야 합니다.
 template<typename T> 
 bool operator==(const debug_alloc<T>& a1, const debug_alloc<T>& a2)
@@ -49,5 +61,14 @@ bool operator!=(const debug_alloc<T>& a1, const debug_alloc<T>& a2)
 
 int main()
 {
-	debug_alloc<int> ax;
+	debug_alloc<Point> ax;
+
+	// 1. 메모리 할당은 allocator 의 allocate 함수 사용
+	Point* p = ax.allocate(1);
+
+	ax.construct(p, 0, 0); // 생성자 호출
+
+	ax.destroy(p);
+
+	ax.deallocate(p, 1);
 }
