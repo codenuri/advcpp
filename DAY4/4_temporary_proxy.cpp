1d36d9ce8b67c0422844a4e40bf7c8d53c28be8d
ecourse <smkang@codenuri.co.kr>
2022-11-24 16:17:50 +0900

    Add files via upload

--- DAY4/4_temporary_proxy.cpp
+++ DAY4/4_temporary_proxy.cpp
@@ -10,5 +10,16 @@ int main()
 	auto a2 = vb[0]; // bool 아닙니다. proxy 입니다.
 
 	std::cout << typeid(a1).name() << std::endl; // int
-	std::cout << typeid(a2).name() << std::endl; // ??
+	std::cout << typeid(a2).name() << std::endl; // proxy!
+
+	for (auto& e : vi) // ok
+	{
+	}
+
+//	for (auto& e : vb)  // error. vb 에서 열거하면 proxy 임시객체로 반환
+						//      임시객체를 rvalue 이므로 auto& 에 담을수 없습니다
+	for (auto&& e : vb) // ok
+	{
+	}
+
 }
