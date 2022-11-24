3502732de2382a001be7b81a461d40dac544a6d3
ecourse <smkang@codenuri.co.kr>
2022-11-24 11:36:36 +0900

    Add files via upload

--- DAY4/1_new9.cpp
+++ DAY4/1_new9.cpp
@@ -10,12 +10,21 @@ int main()
 	auto p = std::remove_if(v.begin(), v.end(), 
 						[](int n) { return n % 2 == 0; });
 
+	std::cout << v.capacity() << ", " << v.size() << std::endl;
+
 	// 2. erase 사용
 	v.erase(p, v.end());
+
+	std::cout << v.capacity() << ", " << v.size() << std::endl;
 
 	// 3. clear() 사용
 	v.clear();
 	
+	std::cout << v.capacity() << ", " << v.size() << std::endl;
+
 	// 4. shrink_to_fit()
 	v.shrink_to_fit();
+
+	std::cout << v.capacity() << ", " << v.size() << std::endl;
 }
+// 위 코드의 각 단계 실행후, capacity 와 size 변화를 생각해보세요.
