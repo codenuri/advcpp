f46f274a032c1cf2c6c449b76118029a252338c8
ecourse <smkang@codenuri.co.kr>
2022-11-24 13:37:42 +0900

    Add files via upload

--- DAY4/2_vector1.cpp
+++ DAY4/2_vector1.cpp
@@ -10,42 +10,57 @@ public:
 	~Point() {}
 };
 
-template<typename T> class vector
+// policy base design(단위 전략 디자인)
+// => 클래스가 사용하는 정책을 템플릿 인자로 변경할수 있게 하는 기술
+
+template<typename T, 
+	     typename Alloc = std::allocator<T> > 
+class vector
 {
 	T* buff;
 	int size;
 	int capacity;
+
+	Alloc ax;	
 public:
 
 	vector(int sz, const T& value) : size(sz), capacity(sz)
 	{
-		buff = static_cast<T*>(operator new(sizeof(T) * sz));
+		buff = ax.allocate(sz);
 
 		int cnt = 0;
 		try
 		{
 			for (int i = 0; i < sz; i++)
 			{
-				new(&buff[i]) T(value);
+				std::allocator_traits<Alloc>::construct(ax,
+												&buff[i], value);
 				cnt = i;
 			}
 		}
 		catch (...)
 		{
 			for (int i = 0; i <= cnt; i++)
-				buff[i].~T();
+			{
+				std::allocator_traits<Alloc>::destroy(ax, &buff[i]);
+			}
 
-			operator delete(buff);
+			ax.deallocate(buff, size);
 			size = 0;
 			capacity = 0;
 		}
 	}
 
-
-
 	~vector()
 	{
-		delete[] buff;
+		for (int i = 0; i < size; i++)
+		{
+			std::allocator_traits<Alloc>::destroy(ax, &buff[i]);
+		}
+
+		ax.deallocate(buff, size);
+		size = 0;
+		capacity = 0;
 	}
 };
 
