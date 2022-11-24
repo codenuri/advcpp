a5180caed37dde15350617e2ab9adbb114645540
ecourse <smkang@codenuri.co.kr>
2022-11-24 15:48:03 +0900

    Add files via upload

---
+++ DAY4/4_temporary_proxy3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+class Label
+{
+	char* text;
+//	int* refcount;
+public:
+	Label(const char* s)
+	{
+		text = new char[strlen(s) + 1];
+		strcpy_s(text, strlen(s) + 1, s);
+	}
+	
+
+
+	/*
+	char& operator[](int idx) 
+	{
+		std::cout << "operator[]" << std::endl;
+		return text[idx]; 
+	}
+	*/
+	class CharProxy
+	{
+		Lable& label;
+		int   idx;
+	public:
+		CharProxy(Lable& b , int n) : label(b), idx(n) {}
+
+		operator char()
+		{
+			printf("b1[0] 이 우변에 있는 경우 - 복사본 만들필요 없다\n");
+			return label.text[idx];
+		}
+	};
+
+	CharProxy operator[](int idx)
+	{
+		std::cout << "operator[]" << std::endl;
+		return CharProxy(*this, idx);
+	}
+};
+
+int main()
+{
+	Label b1("welcome");
+	Label b2 = b1;
+
+	char c = b1[0];	// 임시객체.operator char() 호출됨
+
+	b1[0] = 'X';	
+}
