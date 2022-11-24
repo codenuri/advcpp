18fa35d8df37a625388861177620dabee4540184
ecourse <smkang@codenuri.co.kr>
2022-11-24 14:51:48 +0900

    Add files via upload

---
+++ DAY4/3_rebind2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+template<typename T> class MyAlloc
+{
+public:
+	// C++98 시절의 allocator 에는 반드시 rebind 가 있어야 했습니다.
+	/*
+	template<typename U> struct rebind
+	{
+		typedef MyAlloc<U> other;
+	};
+	*/
+};
+
+template<typename T> void foo(T ax)
+{
+	// C++98 스타일 코드 : allocator 안에 반드시 rebind가 있어야 합니다.
+//	typename T::template rebind<int>::other ax2;
+
+
+	typename std::allocator_traits<T>::template rebind_alloc<int> ax2;
+
+	std::cout << typeid(ax2).name() << std::endl;
+}
+
+int main()
+{
+	MyAlloc<bool> ax;
+	foo(ax);
+}
