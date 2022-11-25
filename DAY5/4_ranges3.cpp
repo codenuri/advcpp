95799afd92f855f95a42b1e0c640a6dd63639f75
ecourse <smkang@codenuri.co.kr>
2022-11-25 15:00:00 +0900

    Add files via upload

---
+++ DAY5/4_ranges3.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+
+// view : 컨테이너를 바라 보는 "시각"
+//		  자원을 소유하지 않은 "range"
+
+template<typename T>
+class reverse_view
+{
+	T& rng;
+public:
+	reverse_view(T& r) : rng(r) {}
+
+	auto begin() { return rng.rbegin(); }
+	auto end() { return rng.rend(); }
+};
+
+template<typename T> 
+class take_view
+{
+	T& rng;
+	int count;
+public:
+	take_view(T& r, int cnt) : rng(r), count(cnt) {}
+
+	auto begin() { return rng.begin(); }
+	auto end()   { return std::next(rng.begin(), count) ; }
+};
+
+int main()
+{
+	std::vector v = { 1, 2, 3, 4, 5 };
+
+	take_view tv(v, 3); // v를 3개만 볼수 있는 view
+
+	for (auto e : tv )
+	{
+		std::cout << e << std::endl;
+	}
+
+}
+
