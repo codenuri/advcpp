1c8a8738f594caf9b3dcf8ad9142730e52242853
ecourse <smkang@codenuri.co.kr>
2022-11-25 09:17:34 +0900

    Add files via upload

--- DAY5/1_any3.cpp
+++ DAY5/1_any3.cpp
@@ -5,11 +5,13 @@ constexpr double pi = 3.14;	// pi 은 double 타입입니다.
 							// pi 를 사용할때는 이름만 적으면 됩니다.
 
 template<typename T> 
-constexpr T pit = 3.14;  // 변수 템플릿
+constexpr T pit = static_cast<T>(3.14);  // 변수 템플릿
 						 // pit 는 템플릿 입니다.
 						 // 사용하려면 pit<타입> 으로 해야 합니다.
 
 int main()
 {
-	double area = 3 * 3 * pi;
+	double area1 = 3 * 3 * pi;
+	double area2 = 3 * 3 * pit<double>;
+	float  area3 = 3 * 3 * pit<float>;
 }
