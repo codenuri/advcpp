45038c473a65cd6b6759f80ef56ab63a1d962ec4
ecourse <smkang@codenuri.co.kr>
2022-11-25 09:33:41 +0900

    Add files via upload

--- DAY5/1_any4.cpp
+++ DAY5/1_any4.cpp
@@ -9,17 +9,33 @@ struct Point
 	Point(int a, int b) : x(a), y(b) {}
 };
 
+struct any
+{
+	void* data;
+
+	const std::type_info& (*get_type)(); // 함수 포인터입니다.
+
+	void(*deleter)(void*);	// 보관하던 객체를 파괴하기 위한 함수 포인터
+
+	template<typename T>
+	any(const T& value) : data( new T(value) )
+	{
+		get_type = []() -> const std::type_info& { return typeid(T); };
+
+		deleter = [](void* data) { delete static_cast<T*>(data); };
+	} 
+
+	~any() { deleter( data ); }
+};
+
 int main()
 {
-	std::string s = "hello";
-
-	any a1 = 10;
-	any a2 = 3.4;
-	any a3 = s;
-
 	Point pt(1, 2);
-	any a4 = pt;
+	any a1 = pt;
 
-//	Point p = any_cast<Point>(a4);
+	std::cout << a1.get_type().name() << std::endl;
+
+
+//	Point p = any_cast<Point>(a1);
 }
 
