526b68dd03a94d139e009a5bac084edfe3673d28
ecourse <smkang@codenuri.co.kr>
2022-11-25 15:58:35 +0900

    Add files via upload

--- DAY5/6_view_interface2.cpp
+++ DAY5/6_view_interface2.cpp
@@ -2,30 +2,40 @@
 #include <iostream>
 #include <ranges> 
 
+// std::ranges::ref_view
+// std::ranges::view_interface
 
+// C++20/23 표준에 많은 view 가 있지만
+// 사용자가 새로운 형태의 view 를 만들어도 됩니다.
+// 이때  되도록이면 "std::ranges::view_interface" 에서 상속 받으세요
+// CRTP 기술이므로 템플릿 인자로 자신의 타입 보내세요
 
 template<typename T>
-class reverse_view : public view_interface< reverse_view<T> >
+class reverse_view : public std::ranges::view_interface< reverse_view<T> >
 {
-	ref_view<T> rng;
+	T rng;
 public:
 	reverse_view(T& r) : rng(r) {}
 
-	auto begin() { return rng.base().rbegin(); }
-	auto end() { return rng.base().rend(); }
+	auto begin() { return rng.rbegin(); }
+	auto end() { return rng.rend(); }
 };
+template<typename T>
+reverse_view(T&&) -> reverse_view<std::views::all_t<T>>;
 
 template<typename T>
-class take_view : public view_interface< take_view<T> >
+class take_view : public std::ranges::view_interface< take_view<T> >
 {
-	ref_view<T> rng;
+	T rng;
 	int count;
 public:
 	take_view(T& r, int cnt) : rng(r), count(cnt) {}
 
-	auto begin() { return rng.base().begin(); }
-	auto end() { return std::next(rng.base().begin(), count); }
+	auto begin() { return rng.begin(); }
+	auto end() { return std::next(rng.begin(), count); }
 };
+template<typename T>
+take_view(T&& t, std::size_t) -> take_view< std::views::all_t<T> >;
 
 int main()
 {
