f7cf349ae5f6d6e79fc89209addf389674b66d6a
ecourse <smkang@codenuri.co.kr>
2022-11-25 16:11:30 +0900

    Add files via upload

--- DAY5/6_view_interface3.cpp
+++ DAY5/6_view_interface3.cpp
@@ -18,10 +18,23 @@ int main()
 		std::cout << e << std::endl;
 	}
 
-	// | 연산자 재정의 되어 있습니다
-
+	// | 연산자 재정의 되어 있습니다. "operator |()" 만든것입니다.
 	auto myview = v | std::views::filter([](int n) { return n % 2 == 0; })
 					| std::views::take(3);  // take_view(앞의결과view, 3);
 
+	// 위 코드는 아래와 같습니다.
+	std::ranges::filter_view fv(v, [](int n) { return n % 2 == 0; });
+	std::ranges::take_view myview2(fv, 3);
+
+	// 핵심 : range 는 지연된 실행입니다.
+	// => 열거할때 적용됩니다.
+
+	v[0] = 100;
+	v[2] = 200;
+
+	for (auto e : myview)
+	{
+		std::cout << e << std::endl; // 2,4,6
+	}
 
 }
